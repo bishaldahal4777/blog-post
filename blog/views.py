633e9743a628a0d80328437cdaf4117e851fcf7f
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-15 18:44:22 +0545

    create post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-
+from .forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
 def post_list(request):
@@ -10,4 +11,16 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success("Post created successfully")
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_create.html', {'form': form})
+
 

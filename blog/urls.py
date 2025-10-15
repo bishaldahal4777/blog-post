158574c4c1acb2bd030849de77f66c34a5e604ea
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-15 18:36:46 +0545

    create post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('posts/', views.post_list, name="post_list" ),
     path('posts/<int:id>', views.post_detail, name="post_detail" ),
+    path('create', views.post_create, name="post_create" ),
+
 
 
 
